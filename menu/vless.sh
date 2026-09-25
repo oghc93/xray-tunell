74b81179e71baf0c17d37bf42640680d0ca47420
oghc93 <scgithub55@gmail.com>
2026-09-25 21:24:13 +0700

    Update vless.sh

--- menu/vless.sh
+++ menu/vless.sh
@@ -33,10 +33,10 @@ vless_header() {
 vless_menu() {
   vless_header
   echo -e "${CYAN}$UI_BOX_TOP${NC}"
-  ui_2col "$(ui_menu_num 1 'Buat Akun' "$GREEN")"    "$(ui_menu_num 2 'Info Akun' "$GREEN")"
-  ui_2col "$(ui_menu_num 3 'Detail Akun' "$GREEN")"  "$(ui_menu_num 4 'Hapus Akun' "$RED")"
-  ui_2col "$(ui_menu_num 5 'Perpanjang Akun')"        "$(ui_menu_num 6 'List Semua Akun' "$CYAN")"
-  ui_2col "$(ui_menu_num 7 'Edit Limit & Kuota')"    "$(ui_menu_num 0 'Kembali' "$DIM")"
+  ui_2col "$(ui_menu_num 01 'Buat Akun' "$GREEN")"    "$(ui_menu_num 02 'Info Akun' "$GREEN")"
+  ui_2col "$(ui_menu_num 03 'Detail Akun' "$GREEN")"  "$(ui_menu_num 04 'Hapus Akun' "$RED")"
+  ui_2col "$(ui_menu_num 05 'Perpanjang Akun')"        "$(ui_menu_num 06 'List Semua Akun' "$CYAN")"
+  ui_2col "$(ui_menu_num 07 'Edit Limit & Kuota')"    "$(ui_menu_num 00 'Kembali' "$DIM")"
   echo -e "${CYAN}$UI_BOX_BOT${NC}"
   echo ""
   echo -ne "  ${WHITE}${BOLD}Pilih [0-7]${NC} ${DIM}›${NC} "
